c5d310ce25f2a076a386920fec1cd8149961cac7
nyk <nykcasseano13@gmail.com>
2022-05-23 21:35:33 -0300

    Projeto de método de coleta de dados  inteligentes

---
+++ metodo.py
@@ -0,0 +1,10 @@
+frequencia = "uma vez na semana"
+intensidade = "pequena"
+
+altemente_frequente = frequencia == "diário"
+print("Usuário altamente ativo:")
+print(altemente_frequente)
+
+altemente_frequente = intensidade == "alto"
+print("Esportes de alta intensidade:")
+print(altemente_frequente)
